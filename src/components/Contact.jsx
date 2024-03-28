cdca3f084f1ccac3fc25767c45a10e823d9c62de
Nichole McGrew <nicholemcgrew@gmail.com>
2024-03-28 14:46:20 -0500

    updated contact gif

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -21,11 +21,11 @@ const Contact = () => {
   return (
     <section
       id="contact"
-      className="contact flex flex-col md:flex-row items-center justify-center mt-8 mb-1 relative z-10"
+      className="contact flex flex-col md:flex-row items-center justify-center mt-8 mb-4 relative z-0"
     >
-      <div className="contact-container flex flex-col md:flex-row items-center justify-center w-full max-w-screen-xl mx-auto space-y-6 md:space-y-0 md:space-x-6">
+      <div className="contact-container flex flex-col md:flex-row items-center justify-center w-full max-w-screen-xl mx-auto space-y-6 md:space-y-0 md:space-x-15">
         {/* Join Us Section */}
-        <div className="max-w-[340px] w-full mx-auto md:w-1/2 p-6 bg-white border rounded-lg shadow-md">
+        <div className="max-w-[340px] w-full mx-auto md:w-1/2 p-6 bg-white border rounded-lg shadow-md relative z-10">
           <h1 className="text-[#003f87] text-4xl font-semibold mb-4">
             Join Us
           </h1>
