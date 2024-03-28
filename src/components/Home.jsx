fa8770d0b65dfa8157f0082eb5b2c70aeb7a34b6
Nichole McGrew <nicholemcgrew@gmail.com>
2024-03-28 10:53:02 -0500

    responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,14 +5,14 @@ const Home = () => {
   return (
     <section
       id="home"
-      className="min-h-screen flex items-center justify-center text-[#003f87] bg-gray-50"
+      className="min-h-screen px-5 sm:px-10 md:px-5 flex items-center justify-center text-[#003f87] bg-gray-50"
     >
-      <div className="mt-10 flex flex-col md:flex-row items-center justify-around w-full max-w-screen-lg md:gap-20 md:p-8">
-        <div className="text-center mb-8 md:mb-0 md:max-w-xl">
+      <div className="flex flex-col md:flex-row items-center justify-around w-full max-w-screen-lg md:gap-20 md:p-8">
+        <div className="text-center mb-8 md:max-w-3xl md:mx-auto">
           <h1 className="text-[#003f87] text-5xl font-semibold mb-10 sm:mt-8 md:mt-4 hover:text-[#ffb225]">
             Welcome to Cub Scouts Pack 365
           </h1>
-          <p className="text-base md:text-lg mb-8 md:mb-8 text-gray whitespace-nowrap overflow-hidden">
+          <p className="text-base md:text-lg mb-8 md:mb-8 text-gray">
             Explore the world of adventure and learning with us!
           </p>
           <ScrollLink
