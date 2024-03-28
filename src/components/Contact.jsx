0896816af7e2d6d92fa7040f0d8f191c8b5e890f
Nichole McGrew <nicholemcgrew@gmail.com>
2024-03-28 14:29:02 -0500

    updated contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -24,6 +24,36 @@ const Contact = () => {
       className="contact flex flex-col md:flex-row items-center justify-center mt-8 mb-1"
     >
       <div className="contact-container flex flex-col md:flex-row items-center justify-center w-full max-w-screen-xl mx-auto">
+        {/* Join Us Section */}
+        <div className="max-w-[340px] w-full mx-auto md:w-1/2 p-6 bg-white border rounded-lg shadow-md mb-10 md:ml-4">
+          <h1 className="text-[#003f87] text-4xl font-semibold mb-4">
+            Join Us
+          </h1>
+          <div className="line mb-7 w-16 border-t-4 border-[#ffb225]"></div>
+          <p className="text-gray-700 mb-4">
+            <FaClock className="inline-block mr-2 text-[#003f87]" />
+            <span className="font-semibold">Meeting Time:</span> Tuesdays at
+            7:00 PM
+          </p>
+          <p className="text-gray-700 mb-4">
+            <FaMapMarkerAlt className="inline-block mr-2 text-[#003f87]" />
+            <span className="font-semibold">Location:</span> Grace Episcopal
+            Church, 600 N Mustang Rd, Yukon, OK, 73099{' '}
+            <a
+              href="https://maps.google.com/?q=Grace+Episcopal+Church,600+N+Mustang+Rd,Yukon,OK,73099"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-[#003f87] hover:underline"
+            >
+              (Google Maps)
+            </a>
+          </p>
+          <p className="text-gray-700 mb-1">
+            Experience the fun and adventure of Cub Scouting. Contact us for
+            more details!
+          </p>
+        </div>
+
         {/* Animation Section */}
         <div className="max-w-[350px] w-full mx-auto md:w-1/2 p-6 bg-white border rounded-lg shadow-md mb-10 md:mr-4">
           <AnimationLottie ref={animationRef} />
@@ -66,36 +96,6 @@ const Contact = () => {
               </a>
             </div>
           </div>
-        </div>
-
-        {/* Join Us Section */}
-        <div className="max-w-[340px] w-full mx-auto md:w-1/2 p-6 bg-white border rounded-lg shadow-md mb-10 md:ml-4 md:mt-0">
-          <h1 className="text-[#003f87] text-4xl font-semibold mb-4">
-            Join Us
-          </h1>
-          <div className="line mb-7 w-16 border-t-4 border-[#ffb225]"></div>
-          <p className="text-gray-700 mb-4">
-            <FaClock className="inline-block mr-2 text-[#003f87]" />
-            <span className="font-semibold">Meeting Time:</span> Tuesdays at
-            7:00 PM
-          </p>
-          <p className="text-gray-700 mb-4">
-            <FaMapMarkerAlt className="inline-block mr-2 text-[#003f87]" />
-            <span className="font-semibold">Location:</span> Grace Episcopal
-            Church, 600 N Mustang Rd, Yukon, OK, 73099{' '}
-            <a
-              href="https://maps.google.com/?q=Grace+Episcopal+Church,600+N+Mustang+Rd,Yukon,OK,73099"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-[#003f87] hover:underline"
-            >
-              (Google Maps)
-            </a>
-          </p>
-          <p className="text-gray-700 mb-1">
-            Experience the fun and adventure of Cub Scouting. Contact us for
-            more details!
-          </p>
         </div>
       </div>
     </section>
