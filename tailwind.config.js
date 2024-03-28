5c0b1375167618e864dfbc7779c4566beff01a5c
Nichole McGrew <nicholemcgrew@gmail.com>
2024-03-28 09:22:26 -0500

    added margin for mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      margin: {
+        'mobile': '1rem', 
+      },
+    },
   },
   plugins: [],
 }
